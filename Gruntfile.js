7e500335007f95d5d4374f6165475c0ffae16247
Tobias Busse <tbusse@poppycircus.fritz.box>
2013-08-03 18:30:01 +0200

    Introduce git-prompt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,26 @@
+var semver = require('semver');
+var getNextVersion = function(version) {
+  var nextVersionArray = version.split('.');
+  nextVersionArray[2] = parseInt(nextVersionArray[2], 10) + 1;
+  nextVersionArray.length = 3;
+  return nextVersionArray.join('.');
+}
+
 module.exports = function(grunt) {
 
   "use strict";
 
+  // Project setup
+  var packageJson = grunt.file.readJSON('package.json');
+  var project = {
+    currentVersion: packageJson.version,
+    nextVersion: getNextVersion(packageJson.version)
+  };
+
   // Project configuration.
   grunt.initConfig({
+    project: project,
+
     jshint: {
       all: ['grunt.js', 'grunt_tasks/*.js', 'src/**/!(lodash|)*.js', 'test/**/*.js'],
       options: {
@@ -60,6 +77,93 @@ module.exports = function(grunt) {
       coverage: ['coverage']
     },
 
+    prompt: {
+      deploy: {
+        options: {
+          questions: [{
+            config:  'deploy.includes',
+            type:    'checkbox',
+            message: 'Artifact description:',
+            choices: [
+              {
+                value: 'NEXUS',
+                name:  'Deploy to nexus'
+              },
+              {
+                value: 'SCM',
+                name:  'Create git tag',
+                checked: true
+              },
+              {
+                value: 'DOC',
+                name:  'Deploy documentation'
+              }
+            ]
+          }, {
+            config: 'deploy.version',
+            type: 'input',
+            message: 'What is the version of the artifact to deploy?',
+            default: project.currentVersion,
+            validate: function(value) {
+              var valid = !!semver.valid(value);
+              var msg = 'Must be a valid semver, such as 1.2.3-rc1. See ' + 'http://semver.org/'.blue.underline + ' for more details.';
+
+              if (valid) {
+                valid = semver.gte(value, project.currentVersion);
+                msg = 'Version not accepted, must be greater than ' + project.currentVersion;
+              }
+
+              //grunt-prompt message seems to be broken
+              if (!valid) {
+                grunt.log.error(msg);
+              }
+
+              project.updatedVersion = value;
+              return valid || msg;
+            }
+          }, {
+            config: 'deploy.tagname',
+            type: 'input',
+            message: 'What is the SCM tag of the artifact to deploy?',
+            default: 'v' + project.currentVersion,
+            validate: function(value) {
+              var valid = value.length > 4;
+              var msg = 'Must be a valid string with more than 4 digits';
+
+              //grunt-prompt message seems to be broken
+              if (!valid) {
+                grunt.log.error(msg);
+              }
+              return valid || msg;
+            },
+            when: function(answers) {
+              return answers['deploy.includes'].indexOf('SCM') > -1;
+            }
+          }, {
+            config: 'deploy.nextVersion',
+            type: 'input',
+            message: 'What is the next version for development?',
+            default: project.nextVersion,
+            validate: function(value) {
+              var valid = !!semver.valid(value);
+              var msg = 'Must be a valid semver, such as 1.2.3-rc1. See ' + 'http://semver.org/'.blue.underline + ' for more details.';
+
+              if (valid) {
+                valid = semver.gt(value, project.updatedVersion);
+                msg = 'Version not accepted, must be greater than ' + project.updatedVersion;
+              }
+
+              //grunt-prompt message seems to be broken
+              if (!valid) {
+                grunt.log.error(msg);
+              }
+              return valid || msg;
+            }
+          }]
+        }
+      },
+    },
+
     requirejs: {
       install: {
         options: {
@@ -87,6 +191,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-jsdoc');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-prompt');
 
   grunt.loadTasks('grunt_tasks');
 
--- grunt_tasks/build.js
+++ grunt_tasks/build.js
@@ -4,9 +4,6 @@ module.exports = function(grunt) {
 
   'use strict';
 
-  //var semver = require('semver');
-  var packageFile = 'package.json';
-
   grunt.registerTask('site', 'Creating site', function() {
     grunt.task.run('jsdoc');
     exec('node node_modules/madge/bin/madge --format amd --exclude "util/lodash|util/class_util" --image doc/dependencies.png src/', function(error, stdout, stderr) {
@@ -33,7 +30,59 @@ module.exports = function(grunt) {
     );
   });
 
-  grunt.registerTask('deploy', 'Deploying couchook', function() {
-    grunt.task.run('install');    
+  grunt.registerTask('deploy-version', 'Deploying couchook - set artifact version', function() {
+    var currentVersion = grunt.config('project.currentVersion');
+    var deployVersion = grunt.config('deploy.version');
+    var asyncDone = this.async();
+
+    if (currentVersion && deployVersion) {
+      if (currentVersion === deployVersion) {
+        grunt.log.ok('Skipped, package.json version (' + deployVersion + ') already up-to-date');
+        asyncDone();
+      } else {
+        var packageJson = grunt.file.readJSON('package.json');
+        packageJson.version = deployVersion;
+        grunt.file.write('package.json', JSON.stringify(packageJson, null, '  '));
+
+        exec([
+          'git add package.json && ',
+          'git commit -m "Version updated to ' + deployVersion + '"'
+        ].join(''), function(error, stdout, stderr) {
+          if (error) {
+            grunt.fatal('Error occurred executing "git add & commit for package.json' + stderr);
+            asyncDone(false);
+          } else {
+            grunt.log.ok('Version updated to ' + deployVersion);
+            asyncDone();
+          }
+        });
+      }
+    } else {
+      grunt.fatal('No version found, use or restart grunt deploy');
+    }
   });
+
+  grunt.registerTask('deploy-tagname', 'Deploying couchook - create git tag', function() {
+    if (!grunt.config('deploy.tagname')) {
+      grunt.log.ok('Skip creating git tag.');
+    } else {
+      grunt.log.ok('Creating git tag.');
+    }
+  });
+
+  grunt.registerTask('deploy-nexus', 'Deploying couchook - copy artifacts to nexus', function() {
+    console.log(grunt.config('deploy.nextVersion'));
+  });
+
+  grunt.registerTask('deploy-next-version', 'Deploying couchook - set development version', function() {
+    console.log(grunt.config('deploy.nextVersion'));
+  });
+
+  grunt.registerTask('deploy', [
+    'prompt:deploy',
+    'deploy-version',
+    'deploy-tagname',
+    'deploy-nexus',
+    'deploy-next-version'
+  ]);
 };
